41e9632220d67581fe531267eeab9d871e2b0127
“Joaquin <joaquingl@ext.inditex.com>
2024-11-12 17:48:20 +0100

    Hibernate I

--- springboot-jpa/src/main/java/com/kim/curso/springboot/jpa/springboot_jpa/SpringbootJpaApplication.java
+++ springboot-jpa/src/main/java/com/kim/curso/springboot/jpa/springboot_jpa/SpringbootJpaApplication.java
@@ -1,11 +1,14 @@
 package com.kim.curso.springboot.jpa.springboot_jpa;
 
+import java.util.Arrays;
 import java.util.List;
+import java.util.Scanner;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.kim.curso.springboot.jpa.springboot_jpa.entities.Person;
 import com.kim.curso.springboot.jpa.springboot_jpa.repositories.PersonRepository;
@@ -23,11 +26,56 @@ public class SpringbootJpaApplication implements CommandLineRunner{
 	@Override
 	public void run(String... args) throws Exception {
 		
-		//List<Person> persons = (List<Person>) repository.findAll();
-		List<Person> persons = (List<Person>) repository.findByProgrammingLanguage("java");
+		list();
+		one();
+		update();
+	}
 
+	@Transactional(readOnly = true)
+	public void list() {
+		//List<Person> persons = (List<Person>) repository.findAll();
+		//List<Person> persons = (List<Person>) repository.findByProgrammingLanguage("java");
+		//List<Person> persons = (List<Person>) repository.buscarByProgrammingLanguage("java");
+		List<Person> persons = repository.findByProgrammingLanguageAndName("java", "Andres");
 
 		persons.stream().forEach(person -> System.out.println(person));
+
+		List<Object[]> personsDatas =  repository.obtenerPersonData();
+
+		personsDatas.stream().forEach(personData -> System.out.println(Arrays.toString(personData)));
+	}
+
+	@Transactional(readOnly = true)
+	public void one() {
+		//repository.findById(2L).ifPresent(person -> System.out.println(person));
+		//repository.findById(2L).ifPresent(System.out::println);
+		//repository.findOne(2L).ifPresent(System.out::println);
+		//repository.findOneName("Pepe").ifPresent(System.out::println);
+		//repository.findOneLikeName("Pe").ifPresent(System.out::println);
+		repository.findByNameContaining("Jos").ifPresent(System.out::println);
+	}
+
+	@Transactional
+	public void create() {
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("Introduce el nombre");
+		String name = scanner.next();
+		System.out.println("Introduce el apellido");
+		String lastname = scanner.next();
+		System.out.println("Introduce el lenguaje de programacion");
+		String programmingLanguage = scanner.next();
+		scanner.close();
+
+		Person person = new Person(null, name, lastname, programmingLanguage);
+		Person personNew = repository.save(person);
+		System.out.println(personNew.toString());
+
+		repository.findById(personNew.getId()).ifPresent(System.out::println);
+	}
+
+	@Transactional
+	public void update() {
+
 	}
 
 }
--- springboot-jpa/src/main/java/com/kim/curso/springboot/jpa/springboot_jpa/repositories/PersonRepository.java
+++ springboot-jpa/src/main/java/com/kim/curso/springboot/jpa/springboot_jpa/repositories/PersonRepository.java
@@ -1,7 +1,9 @@
 package com.kim.curso.springboot.jpa.springboot_jpa.repositories;
 
 import java.util.List;
+import java.util.Optional;
 
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
 import com.kim.curso.springboot.jpa.springboot_jpa.entities.Person;
@@ -9,5 +11,24 @@ import com.kim.curso.springboot.jpa.springboot_jpa.entities.Person;
 public interface PersonRepository extends CrudRepository<Person, Long> {
 
     List<Person> findByProgrammingLanguage(String programmingLanguage);
+
+    @Query("select p from Person p where p.programmingLanguage=?1")
+    List<Person> buscarByProgrammingLanguage(String programmingLanguage);
+
+    List<Person> findByProgrammingLanguageAndName(String programmingLanguage, String name);
+
+    @Query("select p.name, p.programmingLanguage from Person p")
+    List<Object[]> obtenerPersonData();
+
+    @Query("select p From Person p where p.id=?1")
+    Optional<Person> findOne(Long id);
+
+    @Query("select p From Person p where p.name=?1")
+    Optional<Person> findOneName(String name);
+
+    @Query("select p From Person p where p.name like %?1%")
+    Optional<Person> findOneLikeName(String name);
+
+    Optional<Person> findByNameContaining(String name);
 
 }
